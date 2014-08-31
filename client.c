b0819b7e4fe38553b1f2f23914c3230523c644a9
Tomek K <tomicode@gmail.com>
2014-08-31 19:24:45 +0200

    Client overhaul - work on curses

--- client.c
+++ client.c
@@ -31,7 +31,14 @@
 
 #include "chatty.h"
 
+
 static bool recivethread_active = true;
+static WINDOW * message_wnd;
+static WINDOW * input_wnd;
+static WINDOW * status_wnd;
+
+// Function prototypes.
+int client_handler(void);
 
 void close_handler(int sig){
   recivethread_active = false;
@@ -55,7 +62,7 @@ void *reciveThread(void *sid){
       perror("recv");
       break;
 		}
-    printw("%s: %s\n", recv_packet.msg.username, recv_packet.msg.message);
+    wprintw(message_wnd, "%s: %s\n", recv_packet.msg.username, recv_packet.msg.message);
     refresh();
 
     memset(&recv_packet, 0 , sizeof(struct packet));
@@ -65,76 +72,104 @@ void *reciveThread(void *sid){
 
 int client(void)
 {
-  WINDOW * client_wnd; // Main window variable for ncurses init.
-  
-  struct packet client_packet;
-
-  char join_msg[MAX_BUFFER_SIZE];
-  char nick[MAX_NAME_SIZE];
-  char ip[INET6_ADDRSTRLEN], buffer[MAX_BUFFER_SIZE];
-
-  int status, sockid, byte_count;
-  
-  struct addrinfo hints;
-  struct addrinfo *servinfo;
-
-  //pthread_attr_t attr;
-  pthread_t thread;
+  WINDOW * client_wnd; // Main window variable.
+  int lines, cols;
+  int result;
 
   signal(SIGINT, close_handler);
+
+  if((client_wnd = initscr()) == NULL){
+    perror("initscr");
+    exit(EXIT_FAILURE);
+  }
+  getmaxyx(client_wnd, lines, cols); // Get the size of the current terminal window.
+  input_wnd = newwin(1, cols, lines - 1, 0);
+  status_wnd = newwin(1, cols, lines - 2, 0);
+  message_wnd = newwin(lines - 4, cols, 0, 0);
+
+  result = client_handler();
+
+  // Remove all curses windows.
+  delwin(message_wnd);
+  delwin(status_wnd);
+  delwin(input_wnd);
+  delwin(client_wnd);
+  endwin();
+
+  if(result == EXIT_FAILURE){
+    perror("chatty");
+    exit(result);
+  }
+
+  return result;
+}
+
+bool init_colors()
+{
+  if(has_colors() == FALSE){
+    return FALSE;
+  }
+  start_color();
+  // For default color handling of the terminal.
+  use_default_colors();
+  // Initialize color pair sets.
+  init_pair(1, COLOR_WHITE, COLOR_BLUE);
+  return TRUE;
+}
+
+int client_handler()
+{
+  struct packet client_packet; // Main client packet for data sending purpose.
+  struct addrinfo *servinfo;
+  struct addrinfo hints;
+  char nick[MAX_NAME_SIZE];
+  char ip[INET6_ADDRSTRLEN];
+  int status, sockid, byte_count;
+  pthread_t thread;
+
   memset(&hints, 0, sizeof(hints));
   hints.ai_family			= AF_INET; /* use IPv4 */
   hints.ai_socktype		= SOCK_STREAM;
 
-  if((client_wnd = initscr()) == NULL){ // Init the curses window.
-    perror("initscr");
-    exit(EXIT_FAILURE);
-  }
+  keypad(input_wnd, true); // Get all function keys from the keyboard.
+  init_colors();
 
-  keypad(client_wnd, true); // Get all function keys from the keyboard.
-
-  addstr("Chatty - Client Mode\nServer IP: ");
-  refresh();
-  getstr(ip); // Curses function for geting char table.
+  wbkgd(status_wnd, COLOR_PAIR(1));
+  waddstr(input_wnd, "Server IP: ");
+  wgetstr(input_wnd, ip); // Curses function for geting char table.
   if((status = getaddrinfo(ip, PORT, &hints, &servinfo)) != 0){
-    perror("getaddrinfo");
-    exit(EXIT_FAILURE);
+    return EXIT_FAILURE;
   }
-  addstr("Nickname: ");
-  refresh();
-  getstr(nick);
-
+  
+  werase(input_wnd);
+  waddstr(input_wnd, "Username: ");
+  wgetstr(input_wnd, nick);
   if((sockid = socket(servinfo->ai_family, servinfo->ai_socktype, 0)) == -1){
-    perror("socket");
-    exit(EXIT_FAILURE);
+    return EXIT_FAILURE;
   }
 
   if((status = connect(sockid, servinfo->ai_addr, servinfo->ai_addrlen)) != 0){
-      perror("connect");
-      exit(EXIT_FAILURE);
+    return EXIT_FAILURE;
   }
 
-  /* send the JOIN message */
-  /* 6 for "/join " and 1 for the NULL byte */
-  // Client packet struct //
+  // Client packet for connecting the server.
   client_packet.type = PACKET_CMD;
   client_packet.cmd.type = CMD_JOIN;
 
   strcpy(client_packet.cmd.args, nick); 
   if (send(sockid, &client_packet, sizeof(struct packet), 0) == -1){
-    perror("send");
-    exit(EXIT_FAILURE);
+    return EXIT_FAILURE;
   }
 
 	if(pthread_create(&thread, NULL, reciveThread, (void *) &sockid)){
-    perror("pthread");
-    exit(EXIT_FAILURE);
+    return EXIT_FAILURE;
   }
 	while(true){
     memset(&client_packet, 0, sizeof(struct packet));
-
     client_packet.type = PACKET_MSG;
-    getstr(client_packet.msg.message);
+    wgetstr(input_wnd, client_packet.msg.message);
+    werase(input_wnd);
+
     strcpy(client_packet.msg.username, nick);
     if(strcmp(client_packet.msg.message, "/q") == 0){
       memset(&client_packet, 0, sizeof(struct packet));
@@ -142,20 +177,13 @@ int client(void)
     }
 		if(strlen(client_packet.msg.message)){
 			byte_count = send(sockid, &client_packet, sizeof(struct packet), 0);
-      printw("Send %lu bytes [%d]\n", sizeof(struct packet), byte_count);
-      refresh();
+      wprintw(message_wnd, "Send %lu bytes [%d]\n", sizeof(struct packet), byte_count);
+      wrefresh(message_wnd);
 		}
 	}
 
   recivethread_active = false; // Close reciving thread.
   close(sockid); // And close the socket.
-  addstr("\nClosing..");
-  refresh();
-
-  delwin(client_wnd);
-  endwin();
-
-	//free(input);
   freeaddrinfo(servinfo);
   return 0;
 }
